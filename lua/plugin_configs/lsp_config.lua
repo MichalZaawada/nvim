56a832c20a9c0e2d351111de2d9dcadca39a9ed7
Michał Zawada <m.zawada@controlexpert.com>
2023-10-26 16:00:20 +0200

    change in keybindings

--- lua/plugin_configs/lsp_config.lua
+++ lua/plugin_configs/lsp_config.lua
@@ -17,13 +17,13 @@ require('mason-lspconfig').setup({
 lsp_zero.on_attach(function(client, bufnr)
     local opts = {buffer = bufnr, remap = false}
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
     vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
     vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
     vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
@@ -35,8 +35,11 @@ require('lspconfig').omnisharp.setup({
     end,
 })
 
+
 vim.g.OmniSharp_highlighting = 0
 vim.g.OmniSharp_server_use_net6 = 1
+
+require('lspconfig').tsserver.setup({})
 
 lsp_zero.setup()
 
